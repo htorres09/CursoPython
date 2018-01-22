fb95cf908aabd1d19e8d0f8246853b105fd1f7f4
htorres09 <lcc.h.torres09@gmail.com>
2018-01-22 00:45:48 -0600

    1er Ejercicio

---
+++ 1erBloque/ej1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Práctica de Python #1 Script language
+
+cadena = "Hola mundo!"
+
+# MAIN
+if __name__ == "__main__":
+    # Python no proporciona llaves para indicar bloques de código
+    # para definiciones de clases y funciones o control de flujo.
+    # Los bloques de código se denotan por indentación de línea,
+    # que se aplica estrictamente.
+    # El número de espacios en la sangría es variable, pero todas
+    # las declaraciones dentro del bloque deben sangrarse en la misma cantidad.
+    print("Hola mundo!!!")
+
+    """Python permite cadenas de texto que no son asignadas a memoria, como esta.
+    Dichas cadenas sirven para realizar comentarios largos. """
+
+    # El punto y coma (;) permite múltiples instrucciones en una sola línea,
+    # dado que ninguna de las instrucciones inicia un nuevo bloque de código.
+    print("A imprimir: ", cadena); raw_input("\n\nPresióna enter para continuar.")
