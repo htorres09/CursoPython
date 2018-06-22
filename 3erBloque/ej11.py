f3dc6642854cd333ecf0cd8d8abd9b7d99805310
htorres09 <lcc.h.torres09@gmail.com>
2018-06-22 16:46:41 -0500

    Ej 11
    Parcial JSON

---
+++ 3erBloque/ej11.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Práctica de Python #11 Archivos JSON
+
+# Uno de los formatos para guardar y mover información en este momento es JSON
+# Y Python soprta JSON de manera nativa, por lo que ahondaremos un poco en el tema
+# Para iniciar, importamos la libreria <json>
+
+import json
