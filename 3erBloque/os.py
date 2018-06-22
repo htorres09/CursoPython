e96c04d985377aa33d1cdae1253583bb9c51a26e
htorres09 <lcc.h.torres09@gmail.com>
2018-06-22 16:46:18 -0500

    Ej 11
    Subida parcial ej JSON

---
+++ 3erBloque/os.py
@@ -0,0 +1,13 @@
+""" >>> Methods: OS & files
+    os.rename(<archivo>, <nuevo_nombre>)
+                    - Cambia el nombre del archivo por <nuevo_nombre>
+    os.remove(<archivo>)
+                    - Borra el archivo
+    os.mkdir("<nuevo_directorio>")
+                    - Creas una carpeta con el nombre <nuevo_directorio>
+    os.chdir("<nuevo_directorio>")
+                    - Cambias al <nuevo_directorio>
+    os.getcwd()     - Ver el directorio actual
+    os.rmdir("<directorio>")
+                    - Remover el <directorio>
+"""
